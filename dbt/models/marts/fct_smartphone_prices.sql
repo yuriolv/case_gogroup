f3d9cd95ec3bd5cd666fd8ddb9562dcd2aed371a
yuriolv <yurimgl.data@gmail.com>
2026-04-10 14:48:36 -0300

    feat(models): adiciona query para estimativa de vendas

--- dbt/models/marts/fct_smartphone_prices.sql
+++ dbt/models/marts/fct_smartphone_prices.sql
@@ -1,15 +1,51 @@
-select
-    product_id,
-    seller_id,
+with base as (
 
-    date_trunc('day', collected_at) as date,
+    select
+        product_id,
+        title,
+        brand,
+        seller_id,
 
-    price,
-    original_price,
-    discount_pct,
+        date_trunc('day', collected_at) as date_trunc,
+        collected_at,
 
-    stock,
-    free_shipping,
-    condition
+        price,
+        original_price,
+        discount_pct,
 
-from {{ ref('stg_smartphones') }}
+        stock,
+        free_shipping,
+        condition
+
+    from {{ ref('stg_smartphones') }}
+
+),
+
+lagged as (
+
+    select
+        *,
+        lag(stock) over (
+            partition by product_id
+            order by collected_at
+        ) as prev_stock
+
+    from base
+
+),
+
+final as (
+
+    select
+        *,
+        case
+            when prev_stock is null then 0
+            when stock > prev_stock then 0
+            else prev_stock - stock
+        end as sales_estimated
+
+    from lagged
+
+)
+
+select * from final
